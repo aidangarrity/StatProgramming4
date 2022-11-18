a88c39d4dcf2adc7f4194460d87ba8964d7a49a7
EleniMichaelidou <eleni.michaelidou@outlook.com>
2022-11-18 01:14:51 +0000

    small changes

--- proj4.r
+++ proj4.r
@@ -47,18 +47,18 @@ newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,
 ##                            Hi: the inverse of the Hessian matrix at the minimum.
   
 
-  ## approximate the hessian using the gradient if not provided
+  # approximate the hessian using the gradient if not provided
   if(is.null(hess)) {
     hess <- function(theta){
-      grad_0 <- grad(theta, ...) ## compute the gradient vector with the initial parameters
-      n <- length(theta) ## length of the parameter vector (n = # of parameters)
-      hessian <- matrix(0, n, n) ## finite difference Hessian (dimensions: n x n)
-      for (i in 1:n){ ## loop over parameters
-        theta_1 <- theta; theta_1[i] <- theta_1[i] + eps ## increase theta by eps
-        grad_1 <- grad(theta_1, ...) ## recompute the gradient vector with the perturbed parameters
-        hessian[i,] <- (grad_1 - grad_0)/ eps ## approximate second derivatives
+      grad_0 <- grad(theta, ...) # compute the gradient vector with the initial parameters
+      n <- length(theta) # length of the parameter vector (n = # of parameters)
+      hessian <- matrix(0, n, n) # finite difference Hessian (dimensions: n x n)
+      for (i in 1:n){ # loop over parameters
+        theta_1 <- theta; theta_1[i] <- theta_1[i] + eps # increase theta by eps
+        grad_1 <- grad(theta_1, ...) # recompute the gradient vector with the perturbed parameters
+        hessian[i,] <- (grad_1 - grad_0)/ eps # approximate second derivatives
       }
-      hessian <- (t(hessian) + hessian) / 2 ## making the hess matrix symmetric
+      hessian <- (t(hessian) + hessian) / 2 # making the hess matrix symmetric
       return(hessian)
     }
   }
@@ -71,6 +71,27 @@ newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,
     stop("The function or its derivates are not finite at the initial parameters.")
   }
   
+  # Check if the optimization is within the tolerance given the initial guess
+  if (all(abs(grad(theta,...)) < tol * (abs(func(theta, ...)) + fscale))){
+    num_iter <- 0 # number of iterations taken to reach the minimum 
+    
+    # Check if the Hessian is positive definite at convergence. if not warn the user.
+    if (min(eigen(hess(theta, ...))$values) <= 0){
+      warning("The Hessian is not positive definite at convergence and therefore its inverse is not provided.")
+      
+      # Constructing output list
+      out <- list('f'=func(theta, ...),'theta'=theta, 'iter'=num_iter,
+                  'g'=grad(theta, ...), 'Hi'=NULL)
+      
+      return(out)
+    } # exit the function
+    
+    # Constructing output list
+    out <- list('f'=func(theta, ...),'theta'=theta, 'iter'=num_iter,
+                'g'=grad(theta, ...), 'Hi'=chol2inv(chol(hess(theta, ...))))
+    
+    return(out)
+  } # exit the function
   
   theta_k <- theta
   for (iter in 1:maxit) { # loop over the number of Newton iterations to try 
@@ -79,34 +100,73 @@ newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,
     H <- hess(theta_k, ...)
     # flag if the hessian is positive definite
     posdef <- FALSE
-    # Multiple of identity matrix scaled to the order of the Hessian, used to perturb Hessian
-    I <- diag(ncol(H)) * mean(diag(H))
-    k <- 0
+    
+    # Multiple of identity matrix used to perturb Hessian
+    I <- diag(ncol(H)) # create an identity matrix same dim as matrix H
+    i <- 0 # initialize counter for number of trials to perturb the hessian matrix
+    multiple <- 0 # Initialize multiple (set = 0 to check if the initial hessian matrix is positive definite)
+    
+    # # Multiple of identity matrix scaled to the order of the Hessian, used to perturb Hessian
+    # I <- diag(ncol(H)) * mean(diag(H))
+    # k <- 0
+    
     while(!posdef){
-      if (k>10){
-        # Give up if not succesfully perturbed
+      
+      if (i>10){
+        # Give up if not successfully perturbed
         stop("Could not perturb H to be positive definite.")
       }
+      
       # Try cholesky decomposition, will succeed if positive definite.
-      error <- try(chol(H + (10^k)*I),silent=TRUE)
+      error <- try(chol(H + multiple*I),silent=TRUE)
+      
       # Checks to see if the cholesky decomposition failed
-      if (inherits(error, "try-error")){
-        k <- k+1
+      if (inherits(error, "try-error")){ # H is not positive definite
+        
+        if (i==0){ # if the initial hessian matrix is not positive definite then
+          # start perturbing by setting the multiplier to a small multiple
+          # of Hessian matrix inf-norm
+          multiple <- norm(H, c("I")) * 1e-6 
+        }
+        else{ # repeatedly multiply the multiplier by 10
+          multiple <- multiple * 10
+        }
       }
-      else{
+      
+      else{ # H is positive definite
         posdef <- TRUE
-        # If the initial hessian matrix is positive definite, k=0 and it stays the same.
+        # If the initial hessian matrix is positive definite, (i=0 => multiple = 0), then it stays the same.
         # Otherwise we add a multiple of the identity that makes it positive definite.
-        H <- H + 10*k*I
+        H <- H + multiple*I
       }
+      
+      i <- i + 1 # increase counter
+
+      # if (k>10){
+      #   # Give up if not successfully perturbed
+      #   stop("Could not perturb H to be positive definite.")
+      # }
+      # # Try cholesky decomposition, will succeed if positive definite.
+      # error <- try(chol(H + (10^k)*I),silent=TRUE)
+      # # Checks to see if the cholesky decomposition failed
+      # if (inherits(error, "try-error")){
+      #   k <- k+1
+      # }
+      # else{
+      #   posdef <- TRUE
+      #   # If the initial hessian matrix is positive definite, k=0 and it stays the same.
+      #   # Otherwise we add a multiple of the identity that makes it positive definite.
+      #   H <- H + (10^k)*I
+      # }
+      
     }
     
-    stepsize <- 1.0
+    stepsize <- 1.0 # Initial step size
     for (step in 1:max.half){
       # estimate new parameters that would decrease the objective function
+      
       # step = - inv(Hessian matrix) %*% gradient vector
       # - Hessian matrix * step = gradient vector
-      
       # solve with Cholesky
       # compute Cholesky factor of Hessian matrix
       R <- chol(H) # avoid computing twice
@@ -142,22 +202,30 @@ newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,
             number of iterations.")
   }
   
-  # Check if the Hessian is positive definite at convergence. if not warn the user
-  tryCatch(
-    expr = {chol(hess(theta_k,...))} ,
-    error = function(e){
-      message("The Hessian is not positive definite at convergence.")
-    }
-  )
+  # # Check if the Hessian is positive definite at convergence. if not warn the user
+  # tryCatch(
+  #   expr = {chol(hess(theta_k,...))} ,
+  #   error = function(e){
+  #     message("The Hessian is not positive definite at convergence.")
+  #   }
+  # )
+  # 
+  # Check if the Hessian is positive definite at convergence. if not warn the user.
+  if (min(eigen(hess(theta_k, ...))$values) <= 0){
+    warning("The Hessian is not positive definite at convergence and therefore its inverse is not provided.")
+    
+    # Constructing output list
+    out <- list('f'=func(theta_k, ...),'theta'=theta_k, 'iter'=num_iter,
+                'g'=grad(theta_k, ...), 'Hi'= NULL)
+    return(out)
+  }
 
   # Constructing output list
   out <- list('f'=func(theta_k, ...),'theta'=theta_k, 'iter'=num_iter,
               'g'=grad(theta_k, ...), 'Hi'=chol2inv(chol(hess(theta_k, ...))))
   
+  return(out)
   
 } ## newt
 
 ## End of code
-
-
-
